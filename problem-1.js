3d7150a038ae05476f80bb0b9b92d997e7e3d4f7
rakibul875 <rakibulislamashik78@gmail.com>
2026-07-27 17:13:58 +0600

    Solve leetcode problem no-9

---
+++ problem-1.js
@@ -0,0 +1,10 @@
+var isPalindrome = function (x) {
+  const number = x;
+  const numberString = x.toString();
+  const array = numberString.split("");
+  const reverseNumber=array.reverse()
+  const joinNumber = reverseNumber.join("");
+  const isTrue = number === Number(joinNumber);
+  
+};
+
