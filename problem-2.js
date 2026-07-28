2b156bcd408a5946ebf8ce3c3fb0220918e0357a
rakibul875 <rakibulislamashik78@gmail.com>
2026-07-28 20:11:13 +0600

    Refactor formatting in romanToInt function for improved readability

--- problem-2.js
+++ problem-2.js
@@ -1,28 +1,26 @@
-var romanToInt = function(s) {
-    const roman = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    };
+var romanToInt = function (s) {
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
 
-    let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    const current = roman[s[i]];
+    const next = roman[s[i + 1]];
 
-    for (let i = 0; i < s.length; i++) {
-        const current = roman[s[i]];
-        const next = roman[s[i + 1]];
-
-        if (current < next) {
-            total -= current;
-        } else {
-            total += current;
-        }
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
     }
-  
-    return total;
+  }
+  return total;
 };
 
-romanToInt("VII")
+romanToInt("VII");
