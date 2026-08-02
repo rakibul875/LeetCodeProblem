86cc8e53844c6a32252624a3cda9f826086c17b5
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-02 15:34:55 +0600

    Add longestCommonPrefix function to find common prefix in an array of strings

--- mock.js
+++ mock.js
@@ -1,1 +1,13 @@
-console.log('mock')
+var longestCommonPrefix = function (strs) {
+  const len = strs.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i+1; j < strs[i].length; j++) {
+      console.log(strs[i][j]);
+      if (strs[i][j] === strs[i + 1][j]) {
+        return console.log(strs[i].slice(0, j));
+      }
+    }
+  }
+};
+
+longestCommonPrefix(["flower", "flow", "flight"]);
