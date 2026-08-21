0c946190fe4270403349cfce84c5c4836a69c25d
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-21 11:08:12 +0600

    Implement evenSum function to calculate the sum of even numbers in an array

--- problem-5.js
+++ problem-5.js
@@ -1,1 +1,11 @@
-const evenSum = () => {};
+const evenSum = (num) => {
+  let total = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+      total += num[i];
+    }
+  }
+  return total;
+};
+const result = evenSum([10, 5, 10]);
+console.log(result);
