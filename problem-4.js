de7eb1483a6162f5a7b65be2cb0124c8078cd4d1
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-21 10:34:58 +0600

    Refactor lengthOfLastWord function to calculate and log the length of the last word

--- problem-4.js
+++ problem-4.js
@@ -1,5 +1,10 @@
 const lengthOfLastWord = (word) => {
   const trimWord = word.trim();
-  console.log(trimWord);
+  const splitWord = trimWord.split(" ");
+  const lastWord = splitWord[splitWord.length - 1];
+  const lastWordLength = lastWord.length;
+  const result = lastWordLength;
+  console.log(result);
+  return result;
 };
 lengthOfLastWord("    Hello World  ");
