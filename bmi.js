f57f209441d813c9edbed4196ba7c9be562f080f
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-22 21:24:19 +0600

    Add BMI calculation function and result logging in bmi.js

--- bmi.js
+++ bmi.js
@@ -1,2 +1,7 @@
 const weight = process.argv[2];
 const height = process.argv[3];
+const calculateBmi = (weight, height) => {
+  const bmi = weight / (height * height);
+  return bmi;
+};
+const result = calculateBmi(weight, height);
