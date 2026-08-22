6fea5e038599e0cfab8bdccdb6c2f2a0ff5aded6
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-22 20:56:25 +0600

    Add switch statement to evaluate marks and print corresponding grades

---
+++ switch.js
@@ -0,0 +1,12 @@
+const mark = 80;
+switch (true) {
+  case mark >= 80:
+    console.log("A+");
+    break;
+  case mark >= 70:
+    console.log("A");
+    break;
+  default:
+    console.log("improve your study");
+    break;
+}
